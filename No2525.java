4c92704f7a48bef9afd5d4ecc5c9c81ca4f5dac2
jeseong0127 <70552747+jeseong0127@users.noreply.github.com>
2022-05-15 23:28:07 +0900

    No2525

---
+++ No2525.java
@@ -0,0 +1,39 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class No2525 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int hour = scan.nextInt();
+		int minute = scan.nextInt();
+		int time = scan.nextInt();
+
+		if (time > 60) {
+			int resultHour = time / 60;
+			int resultMinute = time % 60;
+			hour += resultHour;
+			minute += resultMinute;
+			
+			if (minute >= 60) {
+				hour += minute / 60;
+				minute = minute % 60;
+			}
+		} else {
+			minute += time;
+			if (minute >= 60) {
+				hour += minute / 60;
+				minute = minute % 60;
+			}
+		}
+		
+		if(hour >= 24) {
+			hour = hour%24;
+		}
+		
+		System.out.println(hour + " " + minute);
+	}
+
+}
