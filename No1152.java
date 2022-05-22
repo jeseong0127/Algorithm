d2ae1aecfb93d07d173771fa4df78234dd83a281
jeseong0127 <70552747+jeseong0127@users.noreply.github.com>
2022-05-22 19:19:49 +0900

    No 1152

---
+++ No1152.java
@@ -0,0 +1,39 @@
+package beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class No1152 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+		
+//		String word = scan.nextLine();
+//		
+//		String[] result = word.split("\\s");
+//		
+		String[] result2 = scan.nextLine().split("\\s");
+		
+		System.out.println(result2.length);
+
+//		다른 풀이방법
+		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//		StringTokenizer st = null;
+//		try {
+//			st = new StringTokenizer(br.readLine(), " ");
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		System.out.print(st.countTokens());
+
+	}
+
+}
